e6ff9471a1177e94601b07172942c5ab005ada6b
Charles Machalow <csm10495@gmail.com>
2024-06-01 11:25:16 -0700

    Add missing mac dependency

--- pre-install-ci.sh
+++ pre-install-ci.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# cheap mac detection
 if which brew; then
+    xcode-select --install
+
     # https://github.com/pymssql/pymssql/issues/372#issuecomment-309950321
     brew install freetds
     brew link --force freetds
